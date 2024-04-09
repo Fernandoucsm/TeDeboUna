9ef32f342a90535a2b15a3525ffcf5982adc82d3
Fernando Cardenas <111361513+Fernandoucsm@users.noreply.github.com>
2024-04-09 14:01:02 -0500

    Registro de usuario

--- TeDeboUna/app/src/main/java/com/example/tedebouna/Registro.java
+++ TeDeboUna/app/src/main/java/com/example/tedebouna/Registro.java
@@ -4,19 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Registro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registro);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -25,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        startActivity(new Intent(MainActivity.this, Registro.class));
+        startActivity(new Intent(Registro.this, MainActivity.class));
     }
+
 }
